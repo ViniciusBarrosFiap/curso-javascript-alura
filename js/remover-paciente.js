2021456c36fa880a67b184e3bc3f1b217832e403
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-04-02 22:42:01 -0300

    Adicionando animacao quando remove paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -6,7 +6,12 @@ tabela.addEventListener("dblclick", function (event) {
   var alvoEvento = event.target
   var paiDoAlvo = alvoEvento.parentNode
 
-  paiDoAlvo.remove()
+  event.target.parentNode.classList.add("fadeOut")
+
+  setTimeout(function () {
+    paiDoAlvo.remove()
+  }, 500)
+
 
 })
 
